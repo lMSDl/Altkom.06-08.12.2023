c02ee9e5d7926672d6096fd800eb3584967729b1
Paweł Łęczycki <pawel@msd.info.pl>
2023-12-08 08:32:48 +0100

    Command

--- ArchitecturalPatterns/Models/SomeModel.cs
+++ ArchitecturalPatterns/Models/SomeModel.cs
@@ -1,13 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ArchitecturalPatterns.Models
 {
-    internal class SomeModel
+    internal class SomeModel : INotifyPropertyChanged
     {
-        public string Value { get; set; }
+        private string _value = "";
+
+        public string Value
+        {
+            get => _value; 
+            set
+            {
+                _value = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Value)));
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
     }
 }
--- ArchitecturalPatterns/ViewModels/ViewModel.cs
+++ ArchitecturalPatterns/ViewModels/ViewModel.cs
@@ -1,21 +1,42 @@
-﻿using ArchitecturalPatterns.Models;
+﻿using ArchitecturalPatterns.Commands;
+using ArchitecturalPatterns.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace ArchitecturalPatterns.ViewModels
 {
-    internal class ViewModel
+    internal class ViewModel : INotifyPropertyChanged
     {
+        private string inputValue = "";
+
         public ViewModel()
         {
-            Model = new SomeModel() { Value = "ala ma kota" };
+            Model = new SomeModel() {  };
+
+            SaveCommand = new Command(x => Model.Value = InputValue, x => Model.Value != InputValue);
+            LoadCommand = new Command(x => InputValue = Model.Value, x => Model.Value != InputValue);
         }
 
         public SomeModel Model { get; set; }
+        public string InputValue
+        {
+            get => inputValue; set
+            {
+                inputValue = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(InputValue)));
 
+                //SaveCommand.RaiseCanExecuteChanged();
+                //LoadCommand.RaiseCanExecuteChanged();
+            }
+        }
+        public ICommand SaveCommand { get; set; }
+        public ICommand LoadCommand { get; set; }
 
+        public event PropertyChangedEventHandler? PropertyChanged;
     }
 }
