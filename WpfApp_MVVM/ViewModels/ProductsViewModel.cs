9fcfc10211ed5f01f91220e4a95ec07dd336964f
Paweł Łęczycki <pawel@msd.info.pl>
2023-12-08 09:48:15 +0100

    WpfApp MVVM

---
+++ WpfApp_MVVM/ViewModels/ProductsViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp_MVVM.ViewModels
+{
+    public class ProductsViewModel : INotifyPropertyChanged
+    {
+        private Product? selectedProduct;
+
+        public ObservableCollection<Product> Products { get; set; } = new ObservableCollection<Product> {};
+
+        public Product? SelectedProduct
+        {
+            get => selectedProduct;
+            set
+            {
+                selectedProduct = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedProduct)));
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
